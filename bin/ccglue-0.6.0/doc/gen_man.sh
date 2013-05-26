4d4fc0fa0ec9b384c5af8043d1bd866bf1fad210
WinDragon <windrg00@gmail.com>
2013-05-26 14:38:10 +0900

    delete ccglue

--- bin/ccglue-0.6.0/doc/gen_man.sh
+++
@@ -1,2 +0,0 @@
-help2man -S "Hari Rangarajan <hariranga@users.sourceforge.net>" --no-info -i ccglue.include ccglue -o ccglue.1
-help2man -S "Hari Rangarajan <hariranga@users.sourceforge.net>" --no-info -i ccglue_tracer.include ccglue_tracer -o ccglue_tracer.1
